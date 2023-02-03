c1a8ebd43fa6f6fba809fdb4d918cce899058f04
sahadatamzad <samzad7698@bths.edu>
2023-02-02 22:11:22 -0500

    finished with general methods, can't figure out why toString only prints out 1 value

--- src/ConverterRunner.java
+++ src/ConverterRunner.java
@@ -3,24 +3,45 @@ import java.util.Arrays;
 
 class ConverterRunner {
     public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
         System.out.println("Welcome to the Number Converter!");
         System.out.println("--------------------------------");
-        System.out.print("Enter the base of your number (2, 8 or 10): ");
-
-        Scanner s = new Scanner(System.in);
+        System.out.println("Option 1. Convert a number of base 2, 8, 10, or 16");
+        System.out.println("Option 2. Convert a decimal number to any base from 1 - 64");
+        System.out.print("Enter the mode you would like to choose (1 or 2): ");
         String choice = s.nextLine();
-        int base = Integer.parseInt(choice);
-
-        System.out.print("Enter your number: ");
-        String number = s.nextLine();
-        int n = Integer.parseInt(number);
-
-        s.close();
-
-        NumberConverter nc = new NumberConverter(n, base);
-        int[] digits = nc.getDigits();
-        System.out.println("\n\nDigit array: " + Arrays.toString(digits));
-        System.out.println("Number: " + nc.displayOriginalNumber());
+        while (!(choice.equals("1") || choice.equals("2"))) {
+            System.out.print("Please enter 1 or 2: ");
+            choice = s.nextLine();
+        }
+        if (choice.equals("1")) {
+            System.out.print("Enter the base of your number (2, 8, 10, or 16): ");
+            String base = s.nextLine();
+            while (!(base.equals("2") || base.equals("8") || base.equals("10") || base.equals("16") )) {
+                System.out.print("Please enter a base of 2, 8, 10, or 16: ");
+                base = s.nextLine();
+            }
+            System.out.print("Enter your number: ");
+            String number = s.nextLine();
+            while (NumberConverter.checkInputs(Integer.parseInt(base), number) == false) {
+                System.out.println(number + " is not a number of base " + base);
+                System.out.print("Please enter a number of base " + base + ": ");
+                number = s.nextLine();
+            }
+            NumberConverter nc = new NumberConverter(number, Integer.parseInt(base));
+            System.out.println(nc.toString());
+        } else if (choice.equals("2")) {
+            System.out.print("Enter your number: ");
+            String number = s.nextLine();
+            while (NumberConverter.checkInputs(10, number) == false) {
+                System.out.print("Please enter a decimal number: ");
+                number = s.nextLine();
+            }
+            System.out.print("Enter the base you would like to convert to (1 - 64): ");
+            String base = s.nextLine();
+            System.out.println(NumberConverter.convertToBase64(Integer.parseInt(number), Integer.parseInt(base)));
+        }
     }
-}
+
+    }
 
--- src/NumberConverter.java
+++ src/NumberConverter.java
@@ -1,14 +1,12 @@
 public class NumberConverter {
-    int[] digits;
-    int base;
+    private String[] digits;
+    private int base;
 
-    public NumberConverter(int number, int base) {
-        String numberAsString = Integer.toString(number);
-        digits = new int[numberAsString.length()];
-        for (int i = 0; i < numberAsString.length(); i++) {
-            String single = numberAsString.substring(i,i+1);
-            int d = Integer.parseInt(single);
-            digits[i] = d;
+    public NumberConverter(String number, int base) {
+        digits = new String[number.length()];
+        for (int i = 0; i < number.length(); i++) {
+            String single = number.substring(i,i+1);
+            digits[i] = single;
         }
         this.base = base;
     }
@@ -22,7 +20,7 @@ public class NumberConverter {
         return o;
     }
 
-    public int[] getDigits() {
+    public String[] getDigits() {
         return digits;
     }
 
@@ -40,12 +38,69 @@ public class NumberConverter {
         return decimalDigits;
     }
 
-    public int[] convertToBinary() {
-        return null;
+    public String[] convertToBinary() {
+        String[] decimalForm = convertToDecimal();
+        int power = 0;
+        String binaryVal = "";
+        int decimalDigit = Integer.parseInt(stringListToString(decimalForm));
+        while (decimalDigit - Math.pow(2, power) >= 0) power++;
+        if (decimalDigit - Math.pow(2, power) < 0) {
+            power--;
+        }
+        int quotient;
+        while (power >= 0) {
+            quotient = decimalDigit / (int) Math.pow(2, power);
+            binaryVal = binaryVal + quotient;
+            decimalDigit -= Math.pow(2, power) * quotient;
+            power--;
+        }
+        String[] binaryDigits = stringToStringList(binaryVal);
+        return binaryDigits;
+
     }
 
-    public int[] convertToOctal() {
-        return null;
+    public String[] convertToOctal() {
+        String[] decimalForm = convertToDecimal();
+        int power = 0;
+        String octalValue = "";
+        int decimalDigit = Integer.parseInt(stringListToString(decimalForm));
+        while (decimalDigit - Math.pow(8, power) >= 0) {
+            power++;
+        }
+        if (decimalDigit - Math.pow(8,power) < 0) {
+            power--;
+        }
+        int quotient;
+        while (power >= 0) {
+            quotient = decimalDigit / (int) Math.pow(8, power);
+            octalValue = octalValue + quotient;
+            decimalDigit -= Math.pow(8, power) * quotient;
+            power--;
+        }
+        String[] octalDigits = stringToStringList(octalValue);
+        return octalDigits;
+    }
+
+    public String[] convertToHexadecimal() {
+        String[] hexaForm = convertToDecimal();
+        int power = 0;
+        String hexaValue = "";
+        int hexaDigit = Integer.parseInt(stringListToString(hexaForm));
+        while (hexaDigit - Math.pow(16, power) >= 0) {
+            power++;
+        }
+        if (hexaDigit - Math.pow(16, power) < 0) {
+            power--;
+        }
+        int quotient;
+        while (power >= 0) {
+            quotient = hexaDigit / (int) Math.pow(8, power);
+            hexaValue = hexaValue + quotient;
+            hexaDigit -= Math.pow(16, power) * quotient;
+            power--;
+        }
+        String[] hexaDigits = stringToStringList(hexaValue);
+        return hexaDigits;
     }
 
     public String stringListToString(String[] stringList) {
@@ -65,7 +120,7 @@ public class NumberConverter {
         return numList;
     }
 
-    public boolean checkInputs(int base, String num) {
+    public static boolean checkInputs(int base, String num) {
         String digit = "";
         String possibleValues = "0123456789ABCDEF";
         String availableBaseValues = possibleValues.substring(0,base);
@@ -80,6 +135,42 @@ public class NumberConverter {
             }
         }
         return true;
+    }
+
+    public static String convertToBase64(int decimal, int base) {
+        String base64Val = "";
+        if (base == 1) {
+            for (int i = 0; i < decimal; i++){
+                base64Val = base64Val + "1";
+            }
+            return base64Val;
+        }
+        else if (decimal == 0) return "0";
+        String allPossibleValues = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz+/";
+        int power = 0;
+        while (decimal - Math.pow(base, power) >= 0) {
+            power++;
+        }
+        if (decimal - Math.pow(base, power) < 0) {
+            power--;
+        }
+        int quotient;
+        while (power >= 0) {
+            quotient = decimal / (int) Math.pow(base, power);
+            base64Val = base64Val + allPossibleValues.substring(quotient, quotient + 1);
+            decimal -= Math.pow(base, power) * quotient;
+            power--;
+        }
+        return base64Val;
+    }
+
+    public String toString() {
+        String str = "";
+        if (base != 2) str += "Binary Value: " +  convertToBase64(Integer.parseInt(stringListToString(convertToDecimal())), 2) + "\r";
+        if (base != 8) str +=  "Octal Value: " + convertToBase64(Integer.parseInt(stringListToString(convertToDecimal())),8) + "\r";
+        if (base != 10) str += "Decimal Value: " + convertToBase64(Integer.parseInt(stringListToString(convertToDecimal())),10) + "\r";
+        if (base != 16) str += "Hexadecimal Value: " + convertToBase64(Integer.parseInt(stringListToString(convertToDecimal())),16);
+        return str;
     }
 
 
